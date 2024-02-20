e86b8e33d12142ed5b87be3ea9d93c9d3e4bc5c2
PravallikaMuvvala <pravallikamuvvala577@gmail.com>
2024-02-20 15:32:44 +0530

    Updated code

---
+++ Sample/sample.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Sample master "<<endl;
+    cout<<"Started"<<endl;
+    cout<<"jhsdddd"<<endl;
+
+}
---
+++ Test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Sample master "<<endl;
+    cout<<"Started"<<endl;
+    cout<<"Test::"<<endl;
+    cout<<"";
+    cout<<"fjaaaaaaadg";
+    
+
+
+}
